e5e95d4559a803191f445928f59d075a630135bd
Trần Trung <nguyentrantrung1986@gmail.com>
2016-02-06 18:16:12 -0800

    count element in array

---
+++ ProccesNumbers/src/proccesnumbers/ProccesNumbers.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proccesnumbers;
+
+/**
+ *
+ * @author TrungNT
+ */
+public class ProccesNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] a = {5, 9, 4, 12, 32, 1, 0};
+        
+        System.out.println("So phan tu trong mang a la: " + a.length);
+    }
+    
+}
